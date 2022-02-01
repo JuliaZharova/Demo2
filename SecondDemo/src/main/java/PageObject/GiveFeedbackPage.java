46cb8b36392ead71ac070da06d64f7afc689b8e8
Julia Zharova <julia.amp.zharova@gmail.com>
2022-02-01 19:44:28 +0200

    Added xml

--- SecondDemo/src/main/java/PageObject/GiveFeedbackPage.java
+++ SecondDemo/src/main/java/PageObject/GiveFeedbackPage.java
@@ -2,9 +2,13 @@ package PageObject;
 
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.support.PageFactory;
+
 public class GiveFeedbackPage extends BasePage{
 
     public GiveFeedbackPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
+
 }
